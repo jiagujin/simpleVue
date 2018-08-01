0b2babfb671d9122cf67dc16ad43424d4f836cd0
jiagujin <623683432@qq.com>
2018-08-01 21:01:49 +0800

    重写依赖收集

--- vue.js
+++ vue.js
@@ -1,19 +1,18 @@
 
 /* 通过 Object.defineProperty 来实现对对象的「响应式」化 */
 function defineReactive(obj,key,val){
-    var dep = new Dep();
+    let dep = new Dep();
+
     Object.defineProperty(obj,key,{
         enumerable: true,       /* 属性可枚举 */
         configurable: true,     /* 属性可被修改或删除 */
         get:function reactiveGetter(){
-            /* 将Dep.target（即当前的Watcher对象存入dep的subs中） */
-            dep.addSub(Dep.target);
+            dep.depend();
             return val;
         },
         set:function reactiveSetter(newVal){
             if(newVal === val) return val;
-            /* 在set的时候触发dep的notify来通知所有的Watcher对象更新视图 */
-            dep.notify();
+            dep.notify()
         }
     })
 }
@@ -27,37 +26,36 @@ function observer(obj){
         defineReactive(obj,key,obj[key]);
     })
 }
-/* 订阅者Dep */
+/*依赖收集中的Dep类 充当发布者的作用*/
 class Dep{
     constructor(){
-        this.subs = [];  //用来存放Watcher对象的数组
+        this.subs = []
     }
-    /* 在subs中添加一个Watcher对象 */
-    addSub(sub){
-        this.subs.push(sub);
+    depend(){
+        if(Dep.target){
+            this.subs.push(Dep.target)
+        }
     }
-    /*通知所有Watcher对象更新视图 */ 
     notify(){
-        this.subs.forEach(sub=>{
-            sub.updata()
+        this.subs.forEach(item=>{
+            item.update()
         })
     }
 }
-/* 观察者Watcher */ 
+/* 依赖收集中的Watcher */
 class Watcher{
     constructor(){
-        /* 在new一个Watcher对象时将该对象赋值给Dep.target，在get中会用到 */
         Dep.target = this;
     }
     /* 更新视图的方法 */
-    update() {
+    update () {
         console.log("视图更新啦～");
     }
 }
 /* 定义Vue类 */
 class Vue{
    constructor(options){
-       this._data = options.data;
-       observer(this._data);
+       this.data = options.data;
+       observer(this.data);
    }
 }
